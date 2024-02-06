efd9b899265c7d56b149d13c3366f25ca2eafc77
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-02-06 19:32:58 +0700

    view(hot-reload): better delay logic

--- public/hot_reload.js
+++ public/hot_reload.js
@@ -1,6 +1,15 @@
 {
-  const eventSource = new EventSource("/hot_reload");
-  eventSource.onmessage = function () {
-    window.location.reload();
-  };
+  let eventSource = null;
+  function connect() {
+    eventSource = new EventSource("/hot_reload");
+    eventSource.onmessage = function () {
+      window.location.reload();
+    };
+    eventSource.onerror = function () {
+      eventSource.close();
+      setTimeout(connect, 50);
+    };
+  }
+
+  connect();
 }
