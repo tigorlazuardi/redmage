accb0efeaffdd47c07bc413064ca8726d9d13e42
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-02-07 16:22:32 +0700

    view(hot-reload): support for multiple tabs

--- app/routes/hot_reload.go
+++ app/routes/hot_reload.go
@@ -2,25 +2,52 @@ package routes
 
 import (
 	"io"
-	"net/http"
+	"sync"
+	"time"
 
 	"github.com/labstack/echo/v5"
 )
 
 func (r *Routes) createHotReloadRoute() func(c echo.Context) error {
-	sseChans := make(map[*http.Request]chan struct{})
+	type hotReloadClient struct {
+		cleaner *time.Timer
+		ch      chan struct{}
+	}
+	var mu sync.Mutex
+	knownClients := make(map[string]hotReloadClient)
+	cleanup := func(id string) *time.Timer {
+		return time.AfterFunc(5*time.Minute, func() {
+			mu.Lock()
+			defer mu.Unlock()
+			delete(knownClients, id)
+		})
+	}
 	return func(c echo.Context) error {
-		ch := make(chan struct{}, 1)
-		sseChans[c.Request()] = ch
+		id := c.Request().URL.Query().Get("id")
+		var ch chan struct{}
+		if oldClient, known := knownClients[id]; known {
+			oldClient.cleaner.Stop()
+			oldClient.cleaner = cleanup(id)
+			ch = oldClient.ch
+		} else {
+			client := hotReloadClient{
+				cleaner: cleanup(id),
+				ch:      make(chan struct{}, 1),
+			}
+			ch = client.ch
+			ch <- struct{}{}
+			knownClients[id] = client
+		}
+
 		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		c.Response().Header().Set("Content-Type", "text/event-stream")
 		c.Response().Header().Set("Cache-Control", "no-cache")
 		c.Response().Header().Set("Connection", "keep-alive")
 		c.Response().WriteHeader(200)
+
 		for {
 			select {
 			case <-c.Request().Context().Done():
-				delete(sseChans, c.Request())
 				return nil
 			case <-r.HotReload:
 				// broadcast to all connected clients
@@ -30,9 +57,11 @@ func (r *Routes) createHotReloadRoute() func(c echo.Context) error {
 				// and the receiver is only one, and chosen at random, so
 				// we have to propagate the signal to all
 				// connected clients.
-				for _, ch := range sseChans {
-					ch <- struct{}{}
+				mu.Lock()
+				for _, ch := range knownClients {
+					ch.ch <- struct{}{}
 				}
+				mu.Unlock()
 			case <-ch:
 				_, err := io.WriteString(c.Response(), "data: Hot reload triggered\n\n")
 				if err != nil {
--- public/hot_reload.js
+++ public/hot_reload.js
@@ -1,12 +1,35 @@
 {
   let eventSource = null;
+  let id;
+
+  const sessions = JSON.parse(
+    localStorage.getItem("hot_reload_sessions") || "{}",
+  );
+
+  for (const key in sessions) {
+    if (!sessions[key].connected) {
+      id = key;
+      sessions[id].connected = true;
+      break;
+    }
+  }
+
+  if (!id) {
+    id = Math.random().toString(36).substring(2);
+    sessions[id] = { connected: true };
+  }
+
+  localStorage.setItem("hot_reload_sessions", JSON.stringify(sessions));
+
   function connect() {
-    eventSource = new EventSource("/hot_reload");
+    eventSource = new EventSource(`/hot_reload?id=${id}`);
     eventSource.onmessage = function () {
       window.location.reload();
     };
     eventSource.onerror = function () {
       eventSource.close();
+      sessions[id].connected = false;
+      localStorage.setItem("hot_reload_sessions", JSON.stringify(sessions));
       setTimeout(connect, 50);
     };
   }
