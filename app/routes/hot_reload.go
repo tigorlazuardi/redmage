fd58de9524d71bcdf2832db1454d6bc300f5179d
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-02-09 10:05:20 +0700

    refactor: removed unnecessary headers

--- app/routes/hot_reload.go
+++ app/routes/hot_reload.go
@@ -21,7 +21,6 @@ func (r *Routes) createHotReloadRoute() func(c echo.Context) error {
 			knownClients[id] = ch
 		}
 
-		c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 		c.Response().Header().Set("Content-Type", "text/event-stream")
 		c.Response().Header().Set("Cache-Control", "no-cache")
 		c.Response().Header().Set("Connection", "keep-alive")
