f74df98a522e6c554a079ecc8c56db08ff707550
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-04-08 16:19:56 +0700

    migration: fix wallpaper and wallpapers order on insert

--- db/migrations/20240407001522_insert_default_subreddits.sql
+++ db/migrations/20240407001522_insert_default_subreddits.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 INSERT INTO subreddits (name, subtype, schedule) VALUES
-('wallpapers', 0, '0 0 * * *'), -- every day at midnight
-('wallpaper', 0, '0 0 * * *'); -- every day at midnight
+('wallpaper', 0, '0 0 * * *'), -- every day at midnight
+('wallpapers', 0, '0 0 * * *'); -- every day at midnight
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DELETE FROM subreddits WHERE name IN ('wallpapers', 'wallpaper');
+DELETE FROM subreddits WHERE name IN ('wallpaper', 'wallpapers');
 -- +goose StatementEnd
