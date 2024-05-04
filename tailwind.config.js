396f426b9cc97ce5b671648894b2c6313122d3f1
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-05-04 15:25:24 +0700

    view: added tailwind scrollbar dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  plugins: [
+    require("daisyui"),
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar")({ noCompatible: true }),
+  ],
   daisyui: {
     themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
