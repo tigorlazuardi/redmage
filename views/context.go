3f3bc72f309683f73d9d184e15ff6979ca0191ca
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-05-12 18:13:00 +0700

    devices-details: added pagination component

--- views/context.go
+++ views/context.go
@@ -1,9 +1,11 @@
 package views
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 
+	"github.com/a-h/templ"
 	"github.com/tigorlazuardi/redmage/config"
 )
 
@@ -19,6 +21,18 @@ func (c *Context) AppendQuery(keyValue ...string) string {
 		query.Add(keyValue[i], keyValue[i+1])
 	}
 	return query.Encode()
+}
+
+// URLWithExtraQuery creates a query based from baseUrl with queries joined between
+// current context and extraQueries.
+//
+// extraKeyValues is an alternating key-value pair.
+func (c *Context) URLWithExtraQuery(baseUrl string, extraKeyValues ...string) templ.SafeURL {
+	query := c.Request.URL.Query()
+	for i := 0; i < len(extraKeyValues); i += 2 {
+		query.Set(extraKeyValues[i], extraKeyValues[i+1])
+	}
+	return templ.SafeURL(fmt.Sprintf("%s?%s", baseUrl, query.Encode()))
 }
 
 func NewContext(config *config.Config, request *http.Request) *Context {
